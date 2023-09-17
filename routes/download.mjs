954ad514959152eda1cdfdc5ac25bd2bd1d6ae01
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-17 18:38:54 +0500

    download api remain to test

--- routes/download.mjs
+++ routes/download.mjs
@@ -2,12 +2,16 @@ import express from "express";
 let router = express.Router();
 import File from "../model/file.mjs";
 
-router.get("/file/download/:uuid", async (req, res) => {
-  const file = await File.findOne({ uuid: req.params.uuid });
+router.get("/files/download/:uuid", async (req, res) => {
+  try {
+    const file = await File.findOne({ uuid: req.params.uuid });
 
-  if (!file) {
-    return res.render("download", { error: "Link has been expired" });
-  }
+    if (!file) {
+      return res.render("download", { error: "Link has been expired" });
+    }
+    const filePath = `${__dirname}/../${file.path}`;
+    res.download(filePath);
+  } catch (error) {}
 });
 
 export default router;
--- routes/show.mjs
+++ routes/show.mjs
@@ -14,7 +14,7 @@ router.get("/showfile/:uuid", async (req, res) => {
       uuid: file.uuid,
       fileName: file.filename,
       fileSize: file.size,
-      downloadLink: `${process.env.APP_BASE_URL}/files/download${file.uuid}`,
+      downloadLink: `${process.env.APP_BASE_URL}/api/files/download/${file.uuid}`,
     });
   } catch (err) {
     return res.render("download", { error: "Something went wrong" });
