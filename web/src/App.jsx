72ca1f087d12fd70da662d8f3ea537165c1482e0
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-26 17:10:14 +0500

    making ui of frontpage

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import Upload from "./components/Upload";
 
 export default function App() {
   return (
-    <div className=" bg-blue-100 min-h-screen p-6">
+    <div className=" bg-black min-h-screen p-6 ">
       <Upload />
     </div>
   );
--- web/src/components/Upload.jsx
+++ web/src/components/Upload.jsx
@@ -74,8 +74,8 @@ const Upload = () => {
     });
 
   return (
-    <div>
-      <div className="p-8 bg-white max-w-2xl m-4 rounded-xl">
+    <div className=" flex border-2 border-emerald-300 justify-center items-center">
+      <div className="p-8 bg-white w-[650px] m-4 rounded-xl">
         <div
           className="  border-blue-400 border-2 border-dashed flex flex-col items-center justify-center gap-4 min-h-[250px] mb-3"
           {...getRootProps()}
